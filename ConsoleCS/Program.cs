864690ae7e136492c6b3e2b9ff6ba2215514dd99
Роман Данилевич <dwordchanel@gmail.com>
2020-04-22 20:18:28 +0300

    Add project files.

---
+++ ConsoleCS/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConsoleCS
+{
+    public class Program
+    {
+        static public int parse(int a)
+        {
+            int c = (a / 100) * 10;
+            c += a % 10;
+            return (c);
+        }
+        static void Main(string[] args)
+        {
+            string S = Console.ReadLine();
+            int a = parse(int.Parse(S));
+            Console.Write("d1 = ");
+            Console.Write(a / 10);
+            Console.Write(", d2 = ");
+            Console.Write(a % 10);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConsoleCS_Test/UnitTest1.cs
@@ -0,0 +1,225 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ProjectCS
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int a = 357;
+            int res = ConsoleCS.Program.parse(a);
+            Assert.AreEqual(37, res);
+        }
+    }
+    [TestClass]
+    public class UnitTest2
+    {
+        [TestMethod]
+        public void TestMethod2()
+        {
+            int a = 945;
+            int res = ConsoleCS.Program.parse(a);
+            Assert.AreEqual(95, res);
+        }
+    }
+    [TestClass]
+    public class UnitTest3
+    {
+        [TestMethod]
+        public void TestMethod3()
+        {
+            int a = 168;
+            int res = ConsoleCS.Program.parse(a);
+            Assert.AreEqual(18, res);
+        }
+    }
+    [TestClass]
+    public class UnitTest4
+    {
+        [TestMethod]
+        public void TestMethod4()
+        {
+            int a = 235;
+            int res = ConsoleCS.Program.parse(a);
+            Assert.AreEqual(25, res);
+        }
+    }
+    [TestClass]
+    public class UnitTest5
+    {
+        [TestMethod]
+        public void TestMethod5()
+        {
+            int a = 778;
+            int res = ConsoleCS.Program.parse(a);
+            Assert.AreEqual(78, res);
+        }
+    }
+    [TestClass]
+    public class UnitTest6
+    {
+        [TestMethod]
+        public void TestMethod6()
+        {
+            int a = 811;
+            int res = ConsoleCS.Program.parse(a);
+            Assert.AreEqual(81, res);
+        }
+    }
+    [TestClass]
+    public class UnitTest7
+    {
+        [TestMethod]
+        public void TestMethod7()
+        {
+            int a = 225;
+            int res = ConsoleCS.Program.parse(a);
+            Assert.AreEqual(25, res);
+        }
+    }
+    [TestClass]
+    public class UnitTest8
+    {
+        [TestMethod]
+        public void TestMethod8()
+        {
+            int a = 100;
+            int res = ConsoleCS.Program.parse(a);
+            Assert.AreEqual(10, res);
+        }
+    }
+    [TestClass]
+    public class UnitTest9
+    {
+        [TestMethod]
+        public void TestMethod9()
+        {
+            int a = 901;
+            int res = ConsoleCS.Program.parse(a);
+            Assert.AreEqual(91, res);
+        }
+    }
+    [TestClass]
+    public class UnitTest10
+    {
+        [TestMethod]
+        public void TestMethod10()
+        {
+            int a = 444;
+            int res = ConsoleCS.Program.parse(a);
+            Assert.AreEqual(44, res);
+        }
+    }
+    [TestClass]
+    public class UnitTest11
+    {
+        [TestMethod]
+        public void TestMethod11()
+        {
+            int a = 120;
+            int res = ConsoleCS.Program.parse(a);
+            Assert.AreEqual(10, res);
+        }
+    }
+    [TestClass]
+    public class UnitTest12
+    {
+        [TestMethod]
+        public void TestMethod12()
+        {
+            int a = 741;
+            int res = ConsoleCS.Program.parse(a);
+            Assert.AreEqual(71, res);
+        }
+    }
+    [TestClass]
+    public class UnitTest13
+    {
+        [TestMethod]
+        public void TestMethod13()
+        {
+            int a = 666;
+            int res = ConsoleCS.Program.parse(a);
+            Assert.AreEqual(66, res);
+        }
+    }
+    [TestClass]
+    public class UnitTest14
+    {
+        [TestMethod]
+        public void TestMethod14()
+        {
+            int a = 522;
+            int res = ConsoleCS.Program.parse(a);
+            Assert.AreEqual(52, res);
+        }
+    }
+    [TestClass]
+    public class UnitTest15
+    {
+        [TestMethod]
+        public void TestMethod15()
+        {
+            int a = 902;
+            int res = ConsoleCS.Program.parse(a);
+            Assert.AreEqual(92, res);
+        }
+    }
+    [TestClass]
+    public class UnitTest16
+    {
+        [TestMethod]
+        public void TestMethod16()
+        {
+            int a = 500;
+            int res = ConsoleCS.Program.parse(a);
+            Assert.AreEqual(50, res);
+        }
+    }
+    [TestClass]
+    public class UnitTest17
+    {
+        [TestMethod]
+        public void TestMethod17()
+        {
+            int a = 229;
+            int res = ConsoleCS.Program.parse(a);
+            Assert.AreEqual(29, res);
+        }
+    }
+    [TestClass]
+    public class UnitTest18
+    {
+        [TestMethod]
+        public void TestMethod18()
+        {
+            int a = 401;
+            int res = ConsoleCS.Program.parse(a);
+            Assert.AreEqual(41, res);
+        }
+    }
+    [TestClass]
+    public class UnitTest19
+    {
+        [TestMethod]
+        public void TestMethod19()
+        {
+            int a = 727;
+            int res = ConsoleCS.Program.parse(a);
+            Assert.AreEqual(77, res);
+        }
+    }
+    [TestClass]
+    public class UnitTest20
+    {
+        [TestMethod]
+        public void TestMethod20()
+        {
+            int a = 903;
+            int res = ConsoleCS.Program.parse(a);
+            Assert.AreEqual(93, res);
+        }
+    }
+}
